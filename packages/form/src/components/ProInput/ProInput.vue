355fcb71b336a9fefd9fd6a86906662b1d58a2bf
hzj <hezhijian249@qq.com>
2024-10-22 23:21:27 +0800

    feat: 简单封装input

---
+++ packages/form/src/components/ProInput/ProInput.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const model = defineModel<string>()
+import { NInput } from 'naive-ui'
+</script>
+
+<template>
+  <NInput v-model:value="model" />
+</template>
+
+<style scoped></style>
