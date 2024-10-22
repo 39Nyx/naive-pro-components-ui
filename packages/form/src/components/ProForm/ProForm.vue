4b26308c0f8087e2038a1951563c58dc7f3ba5a7
hzj <hezhijian249@qq.com>
2024-10-22 23:22:17 +0800

    feat: 简单封装form组件

---
+++ packages/form/src/components/ProForm/ProForm.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { type Ref, ref, watch } from 'vue'
+import { omit } from 'lodash'
+import ProFormFieldRender from '../ProFormFieldRender/ProFormFieldRender'
+import type { ProField } from '../../entity/ProField'
+import { NCard, NForm, NGrid, NGi, NFormItem, NButton, NSpace } from 'naive-ui'
+
+interface Props {
+  columns?: ProField[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  columns: () => [],
+})
+
+const model: Ref<any> = ref({})
+
+watch(
+  () => props.columns,
+  newVal => {
+    if (!newVal) {
+      return
+    }
+    model.value = newVal.reduce((acc, cur) => {
+      acc[cur.key] = ''
+      return acc
+    }, {})
+  },
+  {
+    immediate: true,
+  },
+)
+
+function fieldProps(column: any): ProField {
+  return omit(column, ['key', 'title', 'span'])
+}
+
+function submitForm() {
+  console.log(model.value)
+}
+</script>
+
+<template>
+  <NCard :bordered="false" class="card-wrapper">
+    <NForm label-placement="left" label-width="auto" :model="model">
+      <NGrid cols="24">
+        <NGi
+          v-for="column in props.columns"
+          :key="column.key"
+          :span="column.span || 24"
+        >
+          <NFormItem :label="column.title" :path="column.key">
+            <ProFormFieldRender
+              v-model:value="model[column.key]"
+              v-bind="fieldProps(column)"
+            />
+          </NFormItem>
+        </NGi>
+      </NGrid>
+      <NSpace>
+        <NButton>重置</NButton>
+        <NButton type="primary" @click="submitForm">提交</NButton>
+      </NSpace>
+    </NForm>
+  </NCard>
+</template>
+
+<style scoped lang="less">
+.form-btn {
+  :nth-child(n + 2) {
+    //margin-left: 8px;
+  }
+}
+</style>
