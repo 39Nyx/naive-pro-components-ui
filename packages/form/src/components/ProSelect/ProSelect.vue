17f8bc8e4ec7a78e72c1f48e245d280a82822a8c
hzj <hezhijian249@qq.com>
2024-10-22 23:21:35 +0800

    feat: 简单封装select

---
+++ packages/form/src/components/ProSelect/ProSelect.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { NSelect } from 'naive-ui'
+
+const model = defineModel<string>()
+
+const props = withDefaults(
+  defineProps<{
+    options?: any[]
+  }>(),
+  {
+    options: () => [],
+  },
+)
+</script>
+
+<template>
+  <NSelect v-model:value="model" :options="props.options" />
+</template>
+
+<style scoped></style>
