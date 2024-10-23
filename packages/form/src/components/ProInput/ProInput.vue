0fd7c682d22e594a860309847e9fec74a9e75836
hzj <hezhijian249@qq.com>
2024-10-23 22:54:09 +0800

    feat: proInput添加placeholder、type、disabled、clearable属性配置

--- packages/form/src/components/ProInput/ProInput.vue
+++ packages/form/src/components/ProInput/ProInput.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
-const model = defineModel<string>()
 import { NInput } from 'naive-ui'
+import { ProInputProps } from '../props/ProInputProps'
+import { computed } from 'vue'
+
+const model = defineModel<string>()
+
+const props = withDefaults(defineProps<ProInputProps>(), {
+  type: 'text',
+  disabled: false,
+  clearable: true
+})
+
+const showPlaceholder = computed(() => {
+  return props.placeholder || `请输入${props.title}`
+})
+
 </script>
 
 <template>
-  <NInput v-model:value="model" />
+  <NInput
+    v-model:value="model"
+    :placeholder="showPlaceholder"
+    :type="props.type || 'text'"
+    :disabled="props.disabled"
+    :clearable="props.clearable"
+  />
 </template>
 
 <style scoped></style>
