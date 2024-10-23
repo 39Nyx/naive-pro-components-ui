dfe7c1b285b0bb74d6a7cc2ab59d9c334ee782f9
hzj <hezhijian249@qq.com>
2024-10-23 22:57:57 +0800

    feat: 编写代码测试案例

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
 <script setup lang="ts">
+import { type ProFieldColumn, ProForm } from '@he/naive-pro-components'
+
+const columns: ProFieldColumn[] = [
+  {
+    title: '名称',
+    key: 'name',
+    valueType: 'input',
+    span: 12,
+    type: 'text',
+  },
+  {
+    title: '密码',
+    key: 'password',
+    valueType: 'input',
+    span: 12,
+    type: 'password'
+  },
+  {
+    title: '多行文本',
+    key: 'textarea',
+    valueType: 'input',
+    span: 24,
+    type: 'textarea'
+  },
+  {
+    title: 'Email',
+    key: 'email',
+    valueType: 'input',
+    span: 12,
+  },
+  {
+    title: '下拉框',
+    key: 'phone',
+    valueType: 'select',
+    span: 12,
+    options: [
+      {
+        label: 'Think For Yourself',
+        value: 'song5',
+      },
+      {
+        label: 'The Word',
+        value: 'song6',
+      },
+    ],
+  },
+]
 </script>
 
 <template>
+  <ProForm :columns="columns" />
 </template>
