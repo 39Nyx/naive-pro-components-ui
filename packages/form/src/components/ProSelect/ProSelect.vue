bced8b4e56c9e993ea0fc3e321c6c88f6e9e0634
hzj <hezhijian249@qq.com>
2024-10-23 22:55:42 +0800

    feat: proSelect添加options、placeholders属性配置

--- packages/form/src/components/ProSelect/ProSelect.vue
+++ packages/form/src/components/ProSelect/ProSelect.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 import { NSelect } from 'naive-ui'
+import { computed } from 'vue'
+
+interface ProSelectProps {
+  options: any[]
+  placeholder?: string
+  title?: string
+}
 
 const model = defineModel<string>()
 
-const props = withDefaults(
-  defineProps<{
-    options?: any[]
-  }>(),
-  {
-    options: () => [],
-  },
-)
+const props = withDefaults(defineProps<ProSelectProps>(), {
+  options: () => [],
+  placeholder: '',
+  title: '',
+})
+
+const showPlaceholder = computed(() => {
+  return props.placeholder || `请选择${props.title}`
+})
 </script>
 
 <template>
-  <NSelect v-model:value="model" :options="props.options" />
+  <n-select
+    v-model:value="model"
+    :options="props.options"
+    :placeholder="showPlaceholder"
+  />
 </template>
 
 <style scoped></style>
