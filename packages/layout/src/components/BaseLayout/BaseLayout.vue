3aca7d7373dfae29bc7a928009eef6f48a8d711a
hzj <hezhijian249@qq.com>
2024-10-28 23:52:35 +0800

    feat: 基础布局改为n-layout

--- packages/layout/src/components/BaseLayout/BaseLayout.vue
+++ packages/layout/src/components/BaseLayout/BaseLayout.vue
@@ -1,27 +1,135 @@
 <script setup lang="ts">
+import { h } from 'vue'
 import { RouterView } from 'vue-router'
+import {
+  NLayout,
+  NLayoutSider,
+  NLayoutHeader,
+  NLayoutContent,
+  NLayoutFooter,
+  NMenu,
+  NIcon,
+} from 'naive-ui'
+import { Component } from 'vue'
+
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+} from '@vicons/ionicons5'
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon),
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(BookIcon),
+    disabled: true,
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+      },
+    ],
+  },
+  {
+    label: '寻羊冒险记',
+    key: 'a-wild-sheep-chase',
+    disabled: true,
+    icon: renderIcon(BookIcon),
+  },
+  {
+    label: '舞，舞，舞',
+    key: 'dance-dance-dance',
+    icon: renderIcon(BookIcon),
+    children: [
+      {
+        type: 'group',
+        label: '人物',
+        key: 'people',
+        children: [
+          {
+            label: '叙事者',
+            key: 'narrator',
+            icon: renderIcon(PersonIcon),
+          },
+          {
+            label: '羊男',
+            key: 'sheep-man',
+            icon: renderIcon(PersonIcon),
+          },
+        ],
+      },
+      {
+        label: '饮品',
+        key: 'beverage',
+        icon: renderIcon(WineIcon),
+        children: [
+          {
+            label: '威士忌',
+            key: 'whisky',
+          },
+        ],
+      },
+      {
+        label: '食物',
+        key: 'food',
+        children: [
+          {
+            label: '三明治',
+            key: 'sandwich',
+          },
+        ],
+      },
+      {
+        label: '过去增多，未来减少',
+        key: 'the-past-increases-the-future-recedes',
+      },
+    ],
+  },
+]
 </script>
 
 <template>
-  <div class="relative h-full flex">
-    <aside class="aside">
-
-    </aside>
-    <main class="main">
-      <RouterView />
-    </main>
-  </div>
+  <NLayout has-sider class="layout">
+    <NLayoutSider
+      bordered
+      show-trigger
+      collapse-mode="width"
+      :collapsed-width="64"
+      :width="240"
+      :native-scrollbar="false"
+    >
+      <NMenu
+        :collapsed-width="64"
+        :collapsed-icon-size="22"
+        :options="menuOptions"
+      />
+    </NLayoutSider>
+    <NLayout>
+      <NLayoutHeader>颐和园路</NLayoutHeader>
+      <NLayoutContent content-style="padding: 24px;">
+        <RouterView />
+      </NLayoutContent>
+      <NLayoutFooter>成府路</NLayoutFooter>
+    </NLayout>
+  </NLayout>
 </template>
 
 <style scoped lang="less">
-.aside {
-  width: 220px;
+.layout {
   height: 100%;
-  box-shadow: 0 0 rgb(0 0 0 / 0), 0 0 rgb(0 0 0 / 0), 2px 0 8px 0 rgb(29, 35, 41, 0.05);
 }
 
-.main {
-  flex: 1;
-  height: 100%;
+:deep(.n-layout-sider .n-layout-toggle-button) {
+  top: 80px;
 }
 </style>
