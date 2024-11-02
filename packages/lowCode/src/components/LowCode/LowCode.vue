9f3f6d482b36b8772a20972537680a0eb45fa9d1
hzj <hezhijian249@qq.com>
2024-11-02 19:53:40 +0800

    feat: lowcode页面结构

---
+++ packages/lowCode/src/components/LowCode/LowCode.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DraggablePanel from './components/DraggablePanel/DraggablePanel.vue'
+import SheetPanel from './components/SheetPanel/SheetPanel.vue'
+import PropertiesPanel from './components/PropertiesPanel/PropertiesPanel.vue'
+</script>
+
+<template>
+  <div class="flex h-full">
+    <draggable-panel />
+    <sheet-panel />
+    <properties-panel />
+  </div>
+</template>
+
+<style scoped>
+</style>
