6d363db1681d6f88d719291dfdc81427acd13165
hzj <hezhijian249@qq.com>
2024-11-02 19:52:52 +0800

    feat: 属性面板创建

---
+++ packages/lowCode/src/components/LowCode/components/PropertiesPanel/PropertiesPanel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="properties-panel">
+
+  </div>
+</template>
+
+<style scoped>
+.properties-panel {
+  width: 280px;
+  background: #ffffff;
+}
+</style>
