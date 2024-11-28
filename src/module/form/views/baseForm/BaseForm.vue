d2cd61aca83832bdc18207586aef91cbcd167710
hzj <hezhijian249@qq.com>
2024-11-28 23:09:27 +0800

    feat: 编写baseForm例子

--- src/module/form/views/baseForm/BaseForm.vue
+++ src/module/form/views/baseForm/BaseForm.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
+import { ProForm, type ProFieldColumn } from '@39nyx/naive-pro-components'
 
+const columns: ProFieldColumn[] = [
+  {
+    key: 'age',
+    title: '年龄',
+    valueType: 'input',
+  },
+  {
+    key: 'sex',
+    title: '性别',
+    valueType:'select',
+    options: [
+      {
+        label: '男',
+        value: 'male',
+      },
+      {
+        label: '女',
+        value: 'female',
+      },
+    ],
+  },
+  {
+    key: 'password',
+    title: '密码',
+    valueType: 'input',
+  },
+  {
+    key: 'reenteredPassword',
+    title: '重复密码',
+    valueType: 'input',
+  },
+]
 </script>
 
 <template>
-
+  <pro-form :columns="columns" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
