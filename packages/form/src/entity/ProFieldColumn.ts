35f0b853512c69699e8bc85a67152a17537de067
hzj <hezhijian249@qq.com>
2024-11-30 22:51:12 +0800

    feat  将`ProDatePickerProps` 添加到 `ProFieldColumn` 类型中。

--- packages/form/src/entity/ProFieldColumn.ts
+++ packages/form/src/entity/ProFieldColumn.ts
@@ -1,5 +1,6 @@
 import { type ProField } from './ProField'
 import { type ProInputProps } from '../components/props/ProInputProps'
 import { type ProSelectProps } from '../components/props/ProSelectProps'
+import { ProDatePickerProps } from '../components/props/ProDatePickerProps'
 
-export type ProFieldColumn = ProField & ProInputProps & ProSelectProps
+export type ProFieldColumn = ProField & ProInputProps & ProSelectProps & ProDatePickerProps
