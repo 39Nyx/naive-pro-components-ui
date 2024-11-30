825c49a7cb6cb4e4422c1f59ae2eb68eed247062
hzj <hezhijian249@qq.com>
2024-11-30 22:53:19 +0800

    feat: pro-table集成pro-form

--- packages/table/src/components/ProTable/ProTable.vue
+++ packages/table/src/components/ProTable/ProTable.vue
@@ -1,21 +1,44 @@
 <script setup lang="ts">
-import { NDataTable } from 'naive-ui'
+import { NDataTable, NCard } from 'naive-ui'
 import { type ProTableProps } from '../../props/ProTableProps'
+import { ProForm } from '@39nyx/pro-form'
+import { computed } from 'vue'
 
-withDefaults(defineProps<ProTableProps>(), {
+const props = withDefaults(defineProps<ProTableProps>(), {
   columns: () => [],
   data: () => [],
   pagination: { pageSize: 10 },
 })
+
+const formColumns = computed(() => {
+  return props.columns.map(column => {
+    return {
+      key: column.key,
+      title: column.title,
+      valueType: column?.valueType,
+      span: column.span || 8
+    }
+  })
+})
 </script>
 
 <template>
-  <n-data-table
-    :columns="columns"
-    :data="data"
-    :pagination="pagination"
-    :bordered="false"
+  <pro-form
+    class="pro-form-card"
+    :columns="formColumns"
   />
+  <n-card>
+    <n-data-table
+      :columns="columns"
+      :data="data"
+      :pagination="pagination"
+      :bordered="false"
+    />
+  </n-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pro-form-card {
+  margin-bottom: 16px;
+}
+</style>
