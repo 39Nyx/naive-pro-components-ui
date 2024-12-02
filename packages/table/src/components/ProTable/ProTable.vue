7a3f1eb26e5810b8ae43b7c6d09739c415e1a428
hzj <hezhijian249@qq.com>
2024-12-02 23:25:44 +0800

    feat: ProTable组件新增submitter配置

--- packages/table/src/components/ProTable/ProTable.vue
+++ packages/table/src/components/ProTable/ProTable.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { NDataTable, NCard } from 'naive-ui'
 import { type ProTableProps } from '../../props/ProTableProps'
-import { ProForm } from '@39nyx/pro-form'
-import { computed } from 'vue'
+import { ProForm, type Submitter } from '@39nyx/pro-form'
+import { computed, ref } from 'vue'
 
 const props = withDefaults(defineProps<ProTableProps>(), {
   columns: () => [],
@@ -16,9 +16,16 @@ const formColumns = computed(() => {
       key: column.key,
       title: column.title,
       valueType: column?.valueType,
-      span: column.span || 8
+      span: column.span || 8,
     }
   })
+})
+
+const submitter = ref<Submitter>({
+  searchConfig: {
+    submitText: '查询',
+    resetText: '重置',
+  },
 })
 </script>
 
@@ -26,6 +33,7 @@ const formColumns = computed(() => {
   <pro-form
     class="pro-form-card"
     :columns="formColumns"
+    :submitter="submitter"
   />
   <n-card>
     <n-data-table
