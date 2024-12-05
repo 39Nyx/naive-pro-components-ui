2e141573c1a1bbf2e78129a3a6bbb52e937cf5b1
hzj <hezhijian249@qq.com>
2024-12-05 23:40:10 +0800

    feat: ProToolBarProps新增了columns属性

--- packages/table/src/props/ProToolBarProps.ts
+++ packages/table/src/props/ProToolBarProps.ts
@@ -1,7 +1,10 @@
+import type { ProColumns } from '../entity'
+
 export type ProTableSize = 'large' | 'medium' | 'small'
 
 export type ProToolBarProps = {
   headerTitle?: string
   loading?: boolean
   size?: ProTableSize
+  columns: ProColumns[]
 }
