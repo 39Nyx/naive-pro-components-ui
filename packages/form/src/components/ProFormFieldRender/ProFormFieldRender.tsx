5696402849561c42f5f3d5bbd9f3eb575d32f924
hzj <hezhijian249@qq.com>
2025-07-05 22:10:18 +0800

    feat: [form]更改组件渲染逻辑

--- packages/form/src/components/ProFormFieldRender/ProFormFieldRender.tsx
+++ packages/form/src/components/ProFormFieldRender/ProFormFieldRender.tsx
@@ -4,18 +4,16 @@ import ProInputNumber from '../ProInputNumber/ProInputNumber.vue'
 import ProDatePicker from '../ProDatePicker/ProDatePicker.vue'
 
 const ProFormFieldRender = (props: any) => {
-  let component: any = null
   if (props.valueType === 'input') {
-
-    component = <ProInput {...props} />
+    return <ProInput {...props} />
   } else if (props.valueType === 'select') {
-    component = <ProSelect {...props} />
+    return <ProSelect {...props} />
   } else if (props.valueType === 'inputNumber') {
-    component = <ProInputNumber {...props} />
+    return <ProInputNumber {...props} />
   } else if (props.valueType === 'datePicker') {
-    component = <ProDatePicker {...props} />
+    return <ProDatePicker {...props} />
   }
-  return component
+  return null
 }
 
 export default ProFormFieldRender
