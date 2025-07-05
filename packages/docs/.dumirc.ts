5447a23f9d09a4d1a59755ac481d99cc1731d345
hzj <hezhijian249@qq.com>
2025-07-05 16:42:07 +0800

    feat: 文档的dumirc配置更新

--- packages/docs/.dumirc.ts
+++ packages/docs/.dumirc.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'dumi';
-import { defineThemeConfig } from 'dumi-theme-antd/dist/defineThemeConfig';
 
 export default defineConfig({
   base: '/naive-pro-components-ui/docs/',
@@ -11,61 +10,15 @@ export default defineConfig({
       babelStandaloneCDN: '/naive-pro-components-ui/docs/js/babel.min.js',
     },
   },
-  themeConfig: defineThemeConfig({
-    name: 'naive-pro-components-ui',
+  themeConfig: {
+    name: 'naiveProComponents',
+    logo: '/naive-pro-components-ui/docs/assets/images/AntDesign.png',
     lastUpdated: true,
+    showLineNum: true,
     nav: [
       { title: '研发', link: '/guide/introduce' },
-      { title: '组件', link: '/components/pro-table' },
+      { title: '组件', link: '/comes/form' },
     ],
-    footer: '<span>Copyright © 2025 | Powered by naive pro components</span>',
-    github: 'https://github.com/KuangPF/dumi-theme-antd',
-    sidebarGroupModePath: ['/config', '/components'],
-    title: 'Naive Pro Components',
-    description: 'naive 实现的 pro components',
-    actions: [
-      {
-        type: 'primary',
-        text: '快速开始',
-        link: '/guide/introduce',
-      },
-      {
-        text: '开始使用',
-        link: '/config/base',
-      },
-    ],
-    features: [
-      {
-        title: '简单易用',
-        details: '开箱即用，案例丰富。',
-      },
-      {
-        title: '更高效',
-        details: '傻瓜写法，超高性能, 忽略后半句。',
-      },
-      {
-        title: '更专业',
-        details: '完备，灵活，优雅。',
-      },
-      {
-        title: '场景复用',
-        details: '基于协议驱动，抽象场景组件',
-      },
-      {
-        title: '协议驱动',
-        details: '使用JSON-Schema, 标不标准另说。',
-      },
-      {
-        title: '智能提示',
-        details: '拥抱Typescript',
-      },
-    ],
-    rtl: false,
-    loading: {
-      skeleton: ['/guide', '/config', '/demo'],
-    },
-    docVersions: {
-      '1.0.0': '',
-    },
-  }),
+    github: 'https://github.com/KuangPF/dumi-theme-antd'
+  }
 });
