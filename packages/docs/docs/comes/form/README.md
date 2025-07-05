b8e940b81d84a1b02f787b6faf14e9265c0b805b
hzj <hezhijian249@qq.com>
2025-07-05 17:54:55 +0800

    docs: [docs]基础表单使用说明

---
+++ packages/docs/docs/comes/form/README.md
@@ -0,0 +1,11 @@
+---
+title: Form
+order: 1
+group:
+  title: 数据录入
+  order: 2
+---
+
+## 基础表单
+
+<code src="./demos/BaseFormDemo.vue"></code>
---
+++ packages/docs/docs/comes/form/demos/BaseFormDemo.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { type ProField, ProForm } from '@39nyx/naive-pro-components';
+import { type Ref, ref } from 'vue';
+
+const columns: Ref<ProField[]> = ref([
+  {
+    title: '姓名',
+    key: 'name',
+    valueType: 'input'
+  },
+  {
+    title: '年龄',
+    key: 'age',
+    valueType: 'inputNumber'
+  },
+  {
+    title: '电话号码',
+    key: 'phone',
+    valueType: 'input'
+  }
+])
+
+function mockApiFunction() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ data: '假装成功了' })
+    }, 2000)
+  })
+}
+
+async function onFinish(values: any): Promise<any> {
+  console.log(values)
+  return mockApiFunction().then((res) => {
+    console.log(res)
+  })
+}
+</script>
+
+<template>
+  <pro-form :columns="columns" :on-finish="onFinish">
+  </pro-form>
+</template>
+
+<style scoped>
+
+</style>
