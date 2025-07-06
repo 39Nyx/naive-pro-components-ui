e35137cb3b6bdf6b1623ff491def503241a7d16c
hzj <hezhijian249@qq.com>
2025-07-06 20:48:49 +0800

    feat: [form]propsTransform方法修复类型错误, 改为泛型

--- packages/form/src/utils/propsTransform.ts
+++ packages/form/src/utils/propsTransform.ts
@@ -1,5 +1,3 @@
-import type { ProField } from '../entity'
-
 /**
  * transformKeyMap 用于标记哪些 props 需要被转换
  */
@@ -14,13 +12,13 @@ const transformKeyMap: Record<string, boolean> = {
  * @param props
  * @param data
  */
-export function transformProps(props: ProField, data: any): ProField {
-  return Object.keys(props).reduce((acc: ProField, key: string) => {
-    if (transformKeyMap[key] && props[key] instanceof Function) {
+export function transformProps<T extends Record<string, any>>(props: T, data: any): T {
+  return Object.keys(props).reduce((acc: T, key: keyof T) => {
+    if (transformKeyMap[key as string] && typeof props[key] === 'function') {
       acc[key] = props[key](data)
     } else {
       acc[key] = props[key]
     }
     return acc
-  }, {})
+  }, {} as T)
 }
