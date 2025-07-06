7bf69d17b4b98fc81019ec487584464de3e4afce
hzj <hezhijian249@qq.com>
2025-07-06 23:34:30 +0800

    feat: [form]select支持加载远程数据

--- packages/form/src/components/ProSelect/ProSelect.vue
+++ packages/form/src/components/ProSelect/ProSelect.vue
@@ -1,29 +1,54 @@
 <script setup lang="ts">
-import { NSelect } from 'naive-ui'
-import { computed } from 'vue'
+import { NSelect, NEmpty, NSpin } from 'naive-ui'
+import { computed, type Ref, ref, onMounted } from 'vue'
 import { type ProSelectProps } from '../props/ProSelectProps'
 
 const model = defineModel<string>()
 
 const props = withDefaults(defineProps<ProSelectProps>(), {
-  options: () => [],
   placeholder: '',
   title: '',
   clearable: true,
 })
 
+const loading: Ref<boolean> = ref(false)
+
+type Option = { label: string; value: string }
+
+const removeOptions: Ref<Option[]> = ref([])
+
+const selectOptions = computed(() => {
+  return props.options || removeOptions.value || []
+})
+
 const showPlaceholder = computed(() => {
   return props.placeholder || `请选择${props.title}`
+})
+
+onMounted(() => {
+  if (typeof props.request === 'function') {
+    loading.value = true
+    props.request({}).then((options) => {
+      removeOptions.value = options
+    }).finally(() => {
+      loading.value = false
+    })
+  }
 })
 </script>
 
 <template>
   <n-select
     v-model:value="model"
-    :options="props.options"
+    :options="selectOptions"
     :placeholder="showPlaceholder"
     :clearable="props.clearable"
-  />
+  >
+    <template #empty>
+      <NEmpty v-if="!loading"></NEmpty>
+      <NSpin v-if="loading" :show="loading" :description="'加载中...'" :delay="20"></NSpin>
+    </template>
+  </n-select>
 </template>
 
 <style scoped></style>
--- packages/form/src/components/props/ProSelectProps.ts
+++ packages/form/src/components/props/ProSelectProps.ts
@@ -1,5 +1,16 @@
 import { type ProField } from '../../entity'
 
 export type ProSelectProps = Omit<ProField, 'key'> & {
+  /**
+   * 下拉数据
+   */
   options?: any[]
+
+  /**
+   * 加载远程下拉数据
+   * @param params
+   */
+  request?: <T extends { label: string; value: string }>(
+    params?: any,
+  ) => Promise<T[]>
 }
